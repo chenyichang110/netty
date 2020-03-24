ace3795f692d2f07af98e6fe6f121cc1b94ed796
cyc <cyc>
2020-03-24 11:23:55 +0800

    Buffer测试

--- src/main/java/com/atguigu/nio/BufferTest.java
+++ src/main/java/com/atguigu/nio/BufferTest.java
@@ -1,5 +1,25 @@
 package com.atguigu.nio;
 
+import java.nio.IntBuffer;
+
 public class BufferTest {
 
+  public static void main(String[] args) {
+    //使用IntBuffer测试
+    IntBuffer intBuffer = IntBuffer.allocate(5);
+
+    for (int i = 0; i < intBuffer.capacity(); i++) {
+      intBuffer.put(i * i);
+    }
+
+    //要读取数据时，一定要 调用flip() 方法，读写切换
+    intBuffer.flip();
+
+    intBuffer.position(1);
+    intBuffer.limit(2);
+
+    while (intBuffer.hasRemaining()) {
+      System.out.println(intBuffer.get());
+    }
+  }
 }
